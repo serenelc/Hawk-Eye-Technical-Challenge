86c47e099fa98d3b5f8be87a9ecd630032f633e7
serenechongtrakul <serenelc@outlook.com>
2018-11-13 13:19:18 +0000

    ok pretty sure my get mean and get sum with time is correct

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,10 @@ public class Main {
         test.addData(2);
         test.addData(4);
         test.addData(5);
-        System.out.println("The sum is: " + test.getSum(2, 4));
+        System.out.println("The mean is: " + test.getMean(0, 4));
         java.time.LocalDateTime now = now();
-        java.time.LocalDateTime before = now().minusNanos(10);
-        System.out.println("The sum with time is: " + test.getSumWithTime(before, now));
+        java.time.LocalDateTime before = now().minusMinutes(3);
+        System.out.println("The mean with time is: " + test.getMeanWithTime(before, now));
         //System.out.println("the standard deviation is " + test.getStandardDeviation());
         //System.out.println("Add 6...");
         //test.addData(6);
